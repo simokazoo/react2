97faa05ca8323bf83cde547a3c5c86965c9fb79e
simokazoo <french.simona@yahoo.com>
2019-05-28 21:22:29 +0200

    Updates

---
+++ precache-manifest.a81e7e700f382312577bb4004d128d2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca94a28c7d42b6fac184c565ea6c7b8",
+    "url": "/react2/index.html"
+  },
+  {
+    "revision": "8fd57584a3f82e4fa1af",
+    "url": "/react2/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react2/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8fd57584a3f82e4fa1af",
+    "url": "/react2/static/js/main.2d550d4e.chunk.js"
+  },
+  {
+    "revision": "feafae010b3897100776",
+    "url": "/react2/static/js/runtime~main.04d0f63e.js"
+  }
+]);
